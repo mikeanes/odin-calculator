d0d980abe48f26b8cf0f4c504b0fd41abf1cbd8b
Michael <m.anestopoulos24@gmail.com>
2023-04-27 16:59:21 -0400

    kinda works

--- script.js
+++ script.js
@@ -26,16 +26,41 @@ function operate(num1, operator, num2){
     }
 };
 
+let runningTotal = 0;
+let currentNumber = 0;
+let isEnteringNum1 = true;
+
+const equals = document.getElementById('=');
+const plus = document.getElementById('+');
+
+plus.addEventListener('click', () => {
+   num1 = displayValue;
+   display.textContent = num1;
+   isEnteringNum1 = false;
+    displayValue = 0;
+   
+});
+
+equals.addEventListener('click', () => {
+    if(isEnteringNum1){
+        return;
+    }
+    num2 = displayValue;
+    display.textContent = operate(num1, '+', num2);
+    num1 = num2 = 0;
+    isEnteringNum1 = true;
+});
+
+
 const display = document.getElementById('digits');
 
 const numberButtons = document.querySelectorAll('.key');
 
 let displayValue = 0;
 
-
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        if(display.textContent === '0'){
+        if(display.textContent === '0' || !isEnteringNum1 && display.textContent === num1.toString()){
             display.textContent = '';
         }
         display.textContent += button.textContent;
