0928a2148ca23bb8f2a42ffd03a17be1b06c2b6b
Michael <m.anestopoulos24@gmail.com>
2023-04-27 13:26:50 -0400

    add display and basic calculate functions

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(a, b){
+    return a + b;
+};
+
+function subtract(a, b){
+    return a - b;
+};
+
+function multiply(a, b){
+    return a * b;
+};
+
+function divide(a, b){
+    return a / b;
+};
+
+function operate(num1, operator, num2){
+    if(operator == '+'){
+        return add(num1, num2);
+    }else if(operator == '-'){
+        return subtract(num1, num2);
+    }else if(operator == '*'){
+        return multiply(num1, num2);
+    }else if(operator == '/'){
+        return divide(num1, num2);
+    }
+};
