d2887861aea4ef6e9c81dbc59eab14a48eba077e
Michael <m.anestopoulos24@gmail.com>
2023-05-03 16:03:50 -0400

    does basic 2 number equations_bugs for longer ones

--- script.js
+++ script.js
@@ -28,26 +28,82 @@ function operate(num1, operator, num2){
 
 let runningTotal = 0;
 let currentNumber = 0;
+let operator;
 let inputNewNumber = false;
+let hasEqualClicked = false;
+let secondMinus = false;
 
 const equals = document.getElementById('=');
 const plus = document.getElementById('+');
+const minus = document.getElementById('-');
+const mult = document.getElementById('*');
+const divi = document.getElementById('/');
+
+divi.addEventListener('click', () => {
+    operator = '/';
+    inputNewNumber = true;
+
+    runningTotal = parseInt(display.textContent);
+    if(currentNumber != 0){
+        runningTotal = operate(runningTotal, '*', currentNumber);
+        display.textContent = runningTotal;
+    }
+    if(hasEqualClicked){
+        hasEqualClicked = false;
+    }
+})
+
+mult.addEventListener('click', () => {
+   operator = '*';
+   inputNewNumber = true;
+   
+   runningTotal = parseInt(display.textContent);
+   if(currentNumber != 0){
+   runningTotal = operate(runningTotal, '*', currentNumber);
+   display.textContent = runningTotal;
+   }
+   if(hasEqualClicked){
+    hasEqualClicked = false;
+   }
+});
+
+minus.addEventListener('click', () => {
+   operator = '-';
+   inputNewNumber = true;
+   if(secondMinus){
+    currentNumber = parseInt(display.textContent);
+   }else{
+   runningTotal = parseInt(display.textContent);
+   secondMinus = true;
+   }
+   runningTotal = operate(runningTotal, '-', currentNumber);
+   
+   display.textContent = runningTotal;
+   if(hasEqualClicked){
+    hasEqualClicked = false;
+   }
+});
+
 
 plus.addEventListener('click', () => {
+   operator = '+';
    inputNewNumber = true;
    currentNumber = parseInt(display.textContent);
-   
-   
-   
    runningTotal = operate(runningTotal, '+', currentNumber);
-   currentNumber = 0;
+   
    display.textContent = runningTotal;
+   if(hasEqualClicked){
+    hasEqualClicked = false;
+   }
 });
 
 equals.addEventListener('click', () => {
+    if(!hasEqualClicked){
     currentNumber = parseInt(display.textContent);
-    runningTotal = operate(runningTotal, '+', currentNumber);
+    runningTotal = operate(runningTotal, operator, currentNumber);
     display.textContent = runningTotal;
+    hasEqualClicked = true;
+    }
 });
 
 
@@ -55,12 +111,13 @@ const display = document.getElementById('digits');
 
 const numberButtons = document.querySelectorAll('.key');
 
-let displayValue = 0;
+//let displayValue = 0;
 
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
         if(inputNewNumber){
             display.textContent = '0';
+            inputNewNumber = false;
         }
         if(display.textContent === '0'){
             display.textContent = '';
