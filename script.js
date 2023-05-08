8b1017cbc72331e4b4d161e1e8c97e52151420b5
Michael <m.anestopoulos24@gmail.com>
2023-05-08 11:10:56 -0400

    added keyboard support

--- script.js
+++ script.js
@@ -204,5 +204,87 @@ numberButtons.forEach((button) => {
         display.textContent += button.textContent;
     });
 });
-
+window.onkeydown = function(e){
+    let pressedKey = e.key;
+    let choice;
+    switch(pressedKey){
+        case '0':
+            choice = document.getElementById('0');
+            choice.click();
+            break;
+        case '1':
+            choice = document.getElementById('1');
+            choice.click();
+            break;
+        case '2':
+            choice = document.getElementById('2');
+            choice.click();
+            break;
+        case '3':
+            choice = document.getElementById('3');
+            choice.click();
+            break;
+        case '4':
+            choice = document.getElementById('4');
+            choice.click();
+            break;
+        case '5':
+            choice = document.getElementById('5');
+            choice.click();
+            break;
+        case '6':
+            choice = document.getElementById('6');
+            choice.click();
+            break;
+        case '7':
+            choice = document.getElementById('7');
+            choice.click();
+            break;
+        case '8':
+            choice = document.getElementById('8');
+            choice.click();
+            break;
+        case '9':
+            choice = document.getElementById('9');
+            choice.click();
+            break;
+        case '.':
+            choice = document.getElementById('.');
+            choice.click();
+            break;
+        case '+':
+            choice = document.getElementById('+');
+            choice.click();
+            break;
+        case '-':
+            choice = document.getElementById('-');
+            choice.click();
+            break;
+        case '*':
+            choice = document.getElementById('*');
+            choice.click();
+            break;
+        case '/':
+            choice = document.getElementById('/');
+            choice.click();
+            break;
+        case 'Enter':
+            e.preventDefault();
+            choice = document.getElementById('=');
+            choice.click();
+            break;
+        case '=':
+            choice = document.getElementById('=');
+            choice.click();
+            break;
+        case 'c':
+            choice = document.getElementById('clear');
+            choice.click();
+            break;
+        case 'Backspace':
+            choice = document.getElementById('backspace');
+            choice.click();
+            break;
+    }
+}
 
